10402f322e1cf702eb06429a2f70af3e83e3dffd
vitor-lg <vitorgalhardo12@gmail.com>
2024-12-04 17:49:53 +0100

    build: crear clase cliente

---
+++ Upna_Direct_Entrega/test/upna_direct_Test/ClienteTest.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package upna_direct_Test;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author alumno
+ */
+public class ClienteTest {
+    
+    public ClienteTest() {
+    }
+
+    @Test
+    public void testVerificarCredenciales() {
+    }
+
+    @Test
+    public void testValidarDatosCliente() {
+    }
+    
+}
